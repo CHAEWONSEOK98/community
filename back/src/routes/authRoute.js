f8d29b6e72389fb44b86e50f8a16b1676f4220a3
CHAEWONSEOK98 <7dugod7@naver.com>
2023-11-16 00:30:08 +0900

    back: 로그인 api 생성

--- back/src/routes/authRoute.js
+++ back/src/routes/authRoute.js
@@ -3,6 +3,7 @@ const authRouter = Router();
 const { User } = require('../models/User');
 const bcryptjs = require('bcryptjs');
 const { errorHanlder } = require('../utils/error');
+const jwt = require('jsonwebtoken');
 
 authRouter.post('/signup', async (req, res, next) => {
   try {
@@ -14,6 +15,26 @@ authRouter.post('/signup', async (req, res, next) => {
   } catch (error) {
     // res.status(500).json(error.message);
     // next(errorHanlder(300, 'something went wrong'));
+    next(error);
+  }
+});
+
+authRouter.post('/signin', async (req, res, next) => {
+  const { email, password } = req.body;
+  try {
+    const validUser = await User.findOne({ email });
+    if (!validUser) return next(errorHanlder(404, 'User not found'));
+    const validPassword = bcryptjs.compareSync(password, validUser.password);
+    if (!validPassword) return next(errorHanlder(401, 'wrong credentials'));
+    const { password: hashedPassword, ...rest } = validUser._doc;
+
+    const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
+    const expiryDate = new Date(Date.now() + 3600000); // 1 hour
+    res
+      .cookie('access_token', token, { httpOnly: true, expires: expiryDate })
+      .status(200)
+      .json(rest);
+  } catch (error) {
     next(error);
   }
 });
