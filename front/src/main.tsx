99acb7d2845ffe170a0a9dbe2a590641f4018287
CHAEWONSEOK98 <7dugod7@naver.com>
2023-11-16 14:13:14 +0900

    front: redux-persist 유저 로그인

--- front/src/main.tsx
+++ front/src/main.tsx
@@ -3,13 +3,16 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 
-import { store } from "./store/index.js";
+import { persistor, store } from "./store/index.js";
 import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <PersistGate persistor={persistor} loading={null}>
+        <App />
+      </PersistGate>
     </Provider>
   </React.StrictMode>,
 );
