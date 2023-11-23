dc2fef45b3251f6cb07bd3765b01f53145907d0d
CHAEWONSEOK98 <7dugod7@naver.com>
2023-11-23 20:26:45 +0900

    front: update PostPage ui

--- front/src/components/ModalOverlay.tsx
+++ front/src/components/ModalOverlay.tsx
@@ -24,8 +24,8 @@ const ModalOverlay = ({ setModal }: ModalOverlayProps) => {
     }
   };
   return (
-    <div className="fixed left-1/2 top-1/2 z-20 flex h-48 w-11/12  -translate-x-1/2 -translate-y-1/2 flex-col rounded bg-white px-6 py-8 md:w-96 ">
-      <h2>포스트 삭제</h2>
+    <div className="fixed left-1/2 top-1/2 z-20 flex w-11/12  -translate-x-1/2 -translate-y-1/2 flex-col rounded bg-white px-6 py-8 md:w-96 ">
+      <h2>게시글 삭제</h2>
       <span>정말로 삭제하시겠습니까?</span>
       <div className="flex justify-end space-x-4">
         <button onClick={handleCancel}>취소</button>
--- front/src/pages/PostPage/index.tsx
+++ front/src/pages/PostPage/index.tsx
@@ -1,6 +1,7 @@
 import { Link, useParams } from "react-router-dom";
 import { useEffect, useState } from "react";
 import axios from "axios";
+import { AiOutlineMore } from "react-icons/ai";
 
 import Modal from "../../components/Modal";
 
@@ -14,6 +15,7 @@ interface Post {
 const PostPage = () => {
   const [post, setPost] = useState<Post>([]);
   const [modal, setModal] = useState<boolean>(false);
+  const [toggle, setToggle] = useState<boolean>(false);
   let { postId } = useParams();
 
   useEffect(() => {
@@ -29,27 +31,52 @@ const PostPage = () => {
     setModal(true);
   };
 
+  const handleToggle = () => {
+    setToggle((prev) => !prev);
+  };
+
   return (
-    <div className="flex h-screen items-center justify-center ">
+    <div className="  mx-auto mt-10 max-w-4xl px-4">
       {modal && <Modal setModal={setModal} />}
-      <div className="border-4 p-4">
-        {post &&
-          post.map((element: Post) => {
-            return (
-              <div key={element._id}>
-                <div className="max-w-5xl border-2 p-10">
-                  <h1>{element.title}</h1>
-                  <p>{element.content}</p>
-                  <span>{element.createdAt.slice(0, 10)}</span>
+
+      {post &&
+        post.map((element: Post) => {
+          return (
+            <div key={element._id}>
+              <header className=" flex flex-col gap-2 border-b border-gray-400 pb-4">
+                <h1 className="text-2xl font-bold">{element.title}</h1>
+                <div className="flex items-center gap-2 text-sm  ">
+                  <span className="opacity-70">{element.username}</span>
+                  <span className="opacity-40">l</span>
+                  <span className="opacity-70">
+                    {element.createdAt.slice(0, 10)}{" "}
+                    {element.createdAt.slice(11, 16)}
+                  </span>
+                  <div className="relative">
+                    <AiOutlineMore
+                      onClick={handleToggle}
+                      className="mt-1 h-4 w-4 cursor-pointer rounded-full border border-black"
+                    />
+                    {toggle && (
+                      <div className="absolute  -left-12 top-6 h-20  w-28 rounded-sm border-2  bg-white text-sm">
+                        <div className=" mt-3 flex flex-col items-center gap-2">
+                          <Link to={`/write/${element._id}`}>
+                            <button>수정</button>
+                          </Link>
+                          <button onClick={handleDelete}>삭제</button>
+                        </div>
+                      </div>
+                    )}
+                  </div>
                 </div>
-                <div>
-                  <button onClick={handleDelete}>삭제</button>
-                  <Link to={`/write/${element._id}`}>수정</Link>
-                </div>
-              </div>
-            );
-          })}
-      </div>
+              </header>
+
+              <main className="mt-2">
+                <p>{element.content}</p>
+              </main>
+            </div>
+          );
+        })}
     </div>
   );
 };
