cf0122a32896c1642c0ffd227dd7a215ce320be2
CHAEWONSEOK98 <7dugod7@naver.com>
2023-11-23 13:42:07 +0900

    front: 내 게시글 - 유저와 관계된 게시글 불러오기

--- front/src/pages/MyPage/MyPostListPage.tsx
+++ front/src/pages/MyPage/MyPostListPage.tsx
@@ -1,6 +1,19 @@
 import PostListLayout from "../../components/Layout/PostListLayout";
+import axios from "axios";
+import { useSelector } from "react-redux";
+import { RootState } from "../../store";
+import { useEffect } from "react";
 
 const MyPostListPage = () => {
+  const { currentUser } = useSelector((state: RootState) => state.user);
+  useEffect(() => {
+    (async () => {
+      const data = await axios.post(`http://localhost:3000/post/my`, {
+        userId: currentUser.data._id,
+      });
+      console.log(data);
+    })();
+  }, []);
   return (
     <div className="px-4">
       <h1 className="my-4 font-bold">내 게시글</h1>
