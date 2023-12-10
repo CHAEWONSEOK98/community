55e50ac5da1eb128fd18a2d9832ef3ed1114efad
CHAEWONSEOK98 <7dugod7@naver.com>
2023-12-10 18:57:38 +0900

    update header ui

--- front/src/components/Header.tsx
+++ front/src/components/Header.tsx
@@ -1,6 +1,7 @@
 import { BiSearch } from "react-icons/bi";
 import { BsMoonFill } from "react-icons/bs";
 import { IoIosArrowDown } from "react-icons/io";
+import { HiOutlinePencilSquare } from "react-icons/hi2";
 
 import { Link } from "react-router-dom";
 import { useState } from "react";
@@ -23,25 +24,26 @@ const Header = () => {
   };
 
   return (
-    <header className="sticky top-0 z-10 flex h-24 w-full items-center justify-between border-b-[1px] border-black bg-white px-4 lg:px-0">
-      <div>
-        <Link to={`/`}>
-          <span className=" font-bold">Community</span>
+    <header className="sticky top-0 z-10 flex h-16 w-full items-center justify-between border-b-[1px] border-black bg-white px-4 lg:px-0">
+      <div className="flex w-full items-center">
+        <Link to={`/`} className="pb-1">
+          <p className=" text-xl font-bold">Community</p>
         </Link>
+        <form className="relative ml-auto mr-11 items-center pr-2 md:ml-0 md:flex">
+          <input
+            placeholder="Search"
+            className="ml-2 hidden rounded-3xl  bg-[#F9F9F9] py-3 pl-11 pr-6 text-sm outline-none md:block"
+          />
+          <BiSearch className="absolute left-5 top-1/2 -translate-y-1/2 cursor-pointer text-2xl opacity-70" />
+        </form>
       </div>
 
-      <ul className="relative flex cursor-pointer items-center gap-3">
-        <li className="text-base">
-          <BiSearch />
-        </li>
-
+      <div className="relative flex cursor-pointer items-center gap-3">
         {currentUser ? (
-          <ul className="flex items-center gap-3">
-            <Link
-              to={`/write`}
-              className="hidden rounded-md border border-black p-1 font-bold md:flex"
-            >
-              게시글 작성
+          <div className="flex items-center gap-3">
+            <Link to={`/write`} className="relative hidden md:block">
+              <p className="rounded-md pl-8 opacity-70 md:flex">Write</p>
+              <HiOutlinePencilSquare className="absolute left-0 top-1/2 h-8 w-8 -translate-y-1/2 opacity-50" />
             </Link>
             <div
               onClick={handleToggleClick}
@@ -53,16 +55,16 @@ const Header = () => {
                 alt="profile"
                 className="h-7 w-7 rounded-full object-cover"
               />
-              <IoIosArrowDown />
+              <IoIosArrowDown className="w-5 md:w-8" />
               {userNavigationToggle ? <UserNavigationPanel /> : ""}
             </div>
-          </ul>
+          </div>
         ) : (
           <Link to={`/login`}>
             <li className="text-base">로그인</li>
           </Link>
         )}
-      </ul>
+      </div>
     </header>
   );
 };
--- front/src/components/UserNavigationPanel.tsx
+++ front/src/components/UserNavigationPanel.tsx
@@ -1,8 +1,9 @@
 import { IoIosArrowForward } from "react-icons/io";
-import { BsClipboard } from "react-icons/bs";
+import { HiOutlinePencilSquare } from "react-icons/hi2";
 import { MdOutlineLibraryBooks } from "react-icons/md";
 import { AiOutlineComment } from "react-icons/ai";
 import { AiOutlineLike } from "react-icons/ai";
+import { CiSaveDown2 } from "react-icons/ci";
 import { useAppSelector, useAppDispatch } from "../store";
 import { logOutUser } from "../store/user/userThunkFunction";
 import { Link } from "react-router-dom";
@@ -23,28 +24,34 @@ const UserNavigationPanel = () => {
       <div className="flex flex-col items-center">
         <Link to={`/account/info`}>
           <button className="flex items-center gap-1 rounded-[18px] border p-2 text-xs">
-            <span>{currentUser?.username}님</span>
+            <p>{currentUser?.username}님</p>
             <IoIosArrowForward />
           </button>
         </Link>
 
-        <ul className="mt-4 flex flex-col gap-4">
+        <ul className="mt-4 flex flex-col gap-4 tracking-tighter md:grid md:grid-cols-2">
           <Link to={`/write`} className="flex items-center gap-2 md:hidden">
-            <BsClipboard className="text-xl" />
-            <span className="text-sm">게시글 작성</span>
+            <HiOutlinePencilSquare className="text-xl" />
+            <p className="text-sm">게시글 작성</p>
           </Link>
           <Link to={`/my/post-list`} className="flex items-center gap-2">
             <MdOutlineLibraryBooks className="text-xl" />
-            <span className="text-sm">게시글 내역</span>
+            <p className="text-sm">게시글 내역</p>
           </Link>
           <li className="flex items-center gap-2">
             <AiOutlineComment className="text-xl" />
-            <span className="text-sm">댓글 내역</span>
+            <p className="text-sm">댓글 내역</p>
           </li>
           <Link to={`/my/like-post`}>
             <li className="flex items-center gap-2">
               <AiOutlineLike className="text-xl" />
-              <span className="text-sm">좋아요 게시글</span>
+              <p className="text-sm">좋아요 내역</p>
+            </li>
+          </Link>
+          <Link to={`/my/save`}>
+            <li className="flex items-center gap-2">
+              <CiSaveDown2 className="text-xl" />
+              <p className="text-sm">임시 저장 글</p>
             </li>
           </Link>
         </ul>
